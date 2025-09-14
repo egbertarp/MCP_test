a872cc94ca500b80f3ce6e3fb04c91eb1b6da3bc
egbertarp <egbertarp@yahoo.com>
2025-09-14 14:53:20 +0200

    Add basic test class

---
+++ src/test/java/com/example/mcptest/McpTestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.mcptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
